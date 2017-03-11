b6af179dd87e35ac89d16565c2da7860ef6d1dc4
JBird <cirigj@rpi.edu>
2017-03-11 00:06:07 -0500

    ++ Added helper functions for editor scripting (OnSceneGUI Handles and OnInspectorGUI methods).

--- Scripts/JBirdEditor.cs
+++ Scripts/JBirdEditor.cs
@@ -3,6 +3,7 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -21,8 +22,89 @@ namespace JBirdEngine {
             public ViewOnlyAttribute () { }
         }
 
-		#if UNITY_EDITOR
-		[CustomPropertyDrawer(typeof(ViewOnlyAttribute))]
+        #if UNITY_EDITOR
+
+        public delegate Vector3 PositionHandleDelegate (Vector3 v3, Quaternion quat);
+        public delegate bool ButtonHandleCapDelegate (Vector3 v3, Quaternion quat, float f1, float f2, Handles.CapFunction capFunc);
+        public delegate bool ButtonHandleDrawCapDelegate (Vector3 v3, Quaternion quat, float f1, float f2, Handles.DrawCapFunction drawCapFunc);
+        public delegate Quaternion DiscHandleDelegate (Quaternion quat, Vector3 v31, Vector3 v32, float f1, bool b, float f2);
+        public delegate Quaternion RotationHandleDelegate (Quaternion quat, Vector3 v3);
+        public delegate Vector3 ScaleHandleDelegate (Vector3 v31, Vector3 v32, Quaternion quat, float f);
+        public delegate Vector3 FreeMoveHandleCapDelegate (Vector3 v31, Quaternion quat, float f, Vector3 v32, Handles.CapFunction capFunc);
+        public delegate Vector3 FreeMoveHandleDrawCapDelegate (Vector3 v31, Quaternion quat, float f, Vector3 v32, Handles.DrawCapFunction drawCapFunc);
+        public delegate Quaternion FreeRotateHandleDelegate (Quaternion quat, Vector3 v3, float f);
+        public delegate float RadiusHandleDelegate (Quaternion quat, Vector3 v3, float f);
+        public delegate float RadiusHandleBoolsOnlyDelegate (Quaternion quat, Vector3 v3, float f, bool b);
+        public delegate float ScaleSliderDelegate (float f1, Vector3 v31, Vector3 v32, Quaternion quat, float f2, float f3);
+        public delegate float ScaleValueHandleCapDelegate (float f1, Vector3 v3, Quaternion quat, float f2, Handles.CapFunction capFunc, float f3);
+        public delegate float ScaleValueHandleDrawCapDelegate (float f1, Vector3 v3, Quaternion quat, float f2, Handles.DrawCapFunction drawCapFunc, float f3);
+        public delegate Vector3 SliderDelegate (Vector3 v31, Vector3 v32);
+        public delegate Vector3 SliderCapDelegate (Vector3 v31, Vector3 v32, float f1, Handles.CapFunction capFunc, float f2);
+        public delegate Vector3 SliderDrawCapDelegate (Vector3 v31, Vector3 v32, float f1, Handles.DrawCapFunction drawCapFunc, float f2);
+
+        public static class Utilities {
+
+            public static void HandleCustomEditorMethod (this Editor editor, System.Action method) {
+                Undo.RecordObject(editor.target, string.Format("{0}{1}", editor.target.GetType().ToString(), method.Method.Name));
+                method.Invoke();
+                EditorUtility.SetDirty(editor.target);
+                SceneView.RepaintAll();
+                editor.Repaint();
+            }
+
+            public static void UpdateVariableViaHandle<T> (this Editor editor, ref T var, System.Delegate handleMethod, params object[] handleParams) {
+                UpdateVariableViaHandle(editor, ref var, handleMethod, "", handleParams);
+            }
+
+            public static void UpdateVariableViaHandle<T> (this Editor editor, ref T var, System.Delegate handleMethod, string undoName, params object[] handleParams) {
+                EditorGUI.BeginChangeCheck();
+                T value = default(T);
+                try {
+                    value = (T)handleMethod.DynamicInvoke(handleParams);
+                }
+                catch (System.Exception e) {
+                    Debug.LogErrorFormat("JBirdEditor.UpdateListIndexViaHandle: Method '{0}' does not return type '{1}'. Exception message: {2}", handleMethod.Method.Name, typeof(T).ToString(), e.Message);
+                    EditorGUI.EndChangeCheck();
+                    return;
+                }
+
+                if (EditorGUI.EndChangeCheck()) {
+                    Undo.RecordObject(editor.target, string.Format("{0}Change{1}", editor.target.GetType().ToString(), undoName == "" ? handleMethod.Method.Name : undoName));
+                    var = value;
+                    EditorUtility.SetDirty(editor.target);
+                    SceneView.RepaintAll();
+                    editor.Repaint();
+                }
+            }
+
+            public static void UpdateListIndexViaHandle<T> (this Editor editor, ref List<T> list, int index, System.Delegate handleMethod, params object[] handleParams) {
+                UpdateListIndexViaHandle(editor, ref list, index, handleMethod, "", handleParams);
+            }
+
+            public static void UpdateListIndexViaHandle<T> (this Editor editor, ref List<T> list, int index, System.Delegate handleMethod, string undoName, params object[] handleParams) {
+                EditorGUI.BeginChangeCheck();
+                T value = default(T);
+                try {
+                    value = (T)handleMethod.DynamicInvoke(handleParams);
+                }
+                catch (System.Exception e) {
+                    Debug.LogErrorFormat("JBirdEditor.UpdateListIndexViaHandle: Method '{0}' does not return type '{1}'. Exception message: {2}", handleMethod.Method.Name, typeof(T).ToString(), e.Message);
+                    EditorGUI.EndChangeCheck();
+                    return;
+                }
+
+                if (EditorGUI.EndChangeCheck()) {
+                    Undo.RecordObject(editor.target, string.Format("{0}Change{1}AtIndex{2}", editor.target.GetType().ToString(), undoName == "" ? handleMethod.Method.Name : undoName, index.ToString()));
+                    list[index] = value;
+                    EditorUtility.SetDirty(editor.target);
+                    SceneView.RepaintAll();
+                    editor.Repaint();
+                }
+            }
+
+        }
+
+        [CustomPropertyDrawer(typeof(ViewOnlyAttribute))]
 		public class ViewOnlyDrawer : PropertyDrawer {
 			public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
 				return EditorGUI.GetPropertyHeight(property, label, true);
